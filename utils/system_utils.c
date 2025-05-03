196c077225ff56a58f2ae4a28ddc7b2c4a07016c
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-05-03 14:30:32 +0530

    Update system_utils.c

--- utils/system_utils.c
+++ utils/system_utils.c
@@ -614,7 +614,7 @@ int findPFileAll(char *path, char *search, char **out, int *found_t, int max_lis
         }
         else if(fnmatch(search, entry->d_name, 0) == 0) {
             if(out[*found_t]) {
-                strcpy(out[*found_t], path, strlen(path));
+                strncpy(out[*found_t], path, strlen(path));
                 strncat(out[*found_t], "/", 2);
                 strncat(out[*found_t], entry->d_name, strlen(entry->d_name));
                 (*found_t)++;
