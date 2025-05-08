4314fac835889f46fab990a8daf8cc73727728a0
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-05-08 16:37:04 +0530

    Update system_utils.c

--- utils/system_utils.c
+++ utils/system_utils.c
@@ -598,15 +598,17 @@ int findPFileAll(char *path, char *search, char **out, int *found_t, int max_lis
         if (entry->d_type == DT_DIR) {
             // determinate a full path of an entry
             full_path = calloc(allocate_size, sizeof(char));
-            snprintf(full_path, allocate_size, "%s/%s", path, entry->d_name);
-            SWLOG_INFO(" findPFileAll : Constructed full path: %s", full_path);
-
+            strncpy(full_path, path, allocate_size);
+            full_path[allocate_size- 1] = '\0';
+            strncat(full_path, "/",2);
+            strncat(full_path, entry->d_name, strlen(entry->d_name));
             found = findPFileAll(full_path, search, out, found_t, max_list);
         }
         else if(fnmatch(search, entry->d_name, 0) == 0) {
             if(out[*found_t]) {
-                snprintf(out[*found_t], allocate_size , "%s/%s", path, entry->d_name);
-                SWLOG_INFO(" findPFileAll : Constructed  path, out : %s", full_path);
+                strncpy(out[*found_t], path, strlen(path));
+                strncat(out[*found_t], "/", 2);
+                strncat(out[*found_t], entry->d_name, strlen(entry->d_name));
                 (*found_t)++;
             }
             if((*found_t) >= max_list)
