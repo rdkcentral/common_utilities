cc7e9a692ed01237f53f03547dfd99bfe0677eee
KTirumalaSrihari <102281309+KTirumalaSrihari@users.noreply.github.com>
2025-10-16 16:17:50 +0530

    Add GetLabsignedValue function prototype
    Added GetLabsignedValue function prototype to retrieve LABSIGNED_ENABLED value.

--- utils/common_device_api.h
+++ utils/common_device_api.h
@@ -144,4 +144,15 @@ bool CurrentRunningInst(const char *file);
 
 size_t stripinvalidchar( char *pIn, size_t szIn );
 
+/* function GetLabsignedValue - gets the LABSIGNED_ENABLED value from /etc/device.properties.
+
+        Usage: bool GetLabsignedValue <char> *pBuf, <size_t> szBufSize
+
+            pBuf - pointer to a char buffer to store the output string.
+
+            szBufSize - the size of the character buffer in argument 1.
+
+            RETURN - if firmware version has LABSIGNED and LABSIGNED_ENABLE is true, then TRUE shall be returned. Else, false.
+*/
+bool GetLabsignedValue(char *pBuf, size_t szBufSize);
 #endif
