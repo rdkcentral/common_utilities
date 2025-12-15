5013d06fdb1f7d145b7918ad267e3e2f88054da9
Abhinavpv28 <Abhinav_Valappil@comcast.com>
2025-12-15 18:35:55 +0000

    Update copyright header

--- uploadutils/codebig_upload.c
+++ uploadutils/codebig_upload.c
@@ -13,8 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * SPDX-License-Identifier: Apache-2.0
  */
+
 /**
  * @file codebig_upload.c
  * @brief CodeBig upload implementation with mTLS support
