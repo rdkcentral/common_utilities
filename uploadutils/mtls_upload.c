254e9ec7a0de4f07602dc05ee88f448174be29af
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2026-01-13 14:56:39 +0530

    Update mtls_upload.c

--- uploadutils/mtls_upload.c
+++ uploadutils/mtls_upload.c
@@ -107,7 +107,9 @@ MtlsAuthStatus getCertificateForUpload(MtlsAuth_t *sec, rdkcertselector_h* pthis
 
     COMMONUTILITIES_INFO("[%s:%d] MTLS cert success. cert=%s, type=%s, engine=%s\n",
                __FUNCTION__, __LINE__, sec->cert_name, sec->cert_type, sec->engine);
+#ifndef GTEST_ENABLE
     rdkcertselector_free(pthisCertSel);
+#endif
     
     return MTLS_CERT_FETCH_SUCCESS;
 
@@ -297,4 +299,5 @@ int performMetadataPostWithCertRotationEx(const char *upload_url, const char *fi
 
 
 #endif
+
 
