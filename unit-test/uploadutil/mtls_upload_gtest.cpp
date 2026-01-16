2489edde6d2c3a6e83fc8b7eaa79dd7e7095e730
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2026-01-16 19:52:17 +0530

    Update mtls_upload_gtest.cpp

--- unit-test/uploadutil/mtls_upload_gtest.cpp
+++ unit-test/uploadutil/mtls_upload_gtest.cpp
@@ -1,6 +1,6 @@
 /**
  * Copyright 2025 RDK Management
- * Licensed under the Apache License, Version 2.0
+ * Licensed under the Apache License, Version 2.0;
  *
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
