634246f51b42e9fd0b6eec2e9f43aa1b86143cfd
KTirumalaSrihari <102281309+KTirumalaSrihari@users.noreply.github.com>
2026-08-16 19:54:04 +0530

    Update common_device_api.h

--- utils/common_device_api.h
+++ utils/common_device_api.h
@@ -59,6 +59,8 @@
 #define TIMEZONE_DST_FILE       "/tmp/timeZoneDST"
 #define TIMEZONE_OFFSET_MAP     "/tmp/timeZone_offset_map"
 #define BUF_MAX_LEN             64
+#define RFC_DEBUGSRV            "Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Identity.DbgServices.Enable"
+#define RFC_DEVICETYPE          "Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Identity.DeviceType"
 
 #endif
 
